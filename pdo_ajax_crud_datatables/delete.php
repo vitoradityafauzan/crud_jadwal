d026a298869834932dbe71c86439330a33172b6a
Vito Raditya Fauzan <vitoraditya.f@gmail.com>
2018-03-13 08:05:39 +0700

    MK-1
    .

---
+++ pdo_ajax_crud_datatables/delete.php
@@ -0,0 +1,30 @@
+<?php
+
+include('db.php');
+include("function.php");
+
+if(isset($_POST["id_job"]))
+{
+	$image = get_image_name($_POST["id_job"]);
+	if($image != '')
+	{
+		unlink("upload/" . $image);
+	}
+	$statement = $connection->prepare(
+		"DELETE FROM jobs WHERE id_job = :id_job"
+	);
+	$result = $statement->execute(
+		array(
+			':id_job'	=>	$_POST["id_job"]
+		)
+	);
+	
+	if(!empty($result))
+	{
+		echo 'Data Deleted';
+	}
+}
+
+
+
+?>
